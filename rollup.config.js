b33ae96d7d50a40a879fb7d9b0cf57a115a03fbd
soshiharami <soshi0807sss@gmail.com>
2020-11-03 20:52:23 +0900

    :add: dev server

--- rollup.config.js
+++ rollup.config.js
@@ -22,6 +22,27 @@ const opts = {
   }
 };
 
+function serve() {
+  let server;
+
+  function toExit() {
+    if (server) server.kill(0);
+  }
+
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      });
+
+      process.on('SIGTERM', toExit);
+      process.on('exit', toExit);
+    }
+  };
+}
+
 export default {
   input: "src/main.ts",
   output: {
@@ -53,6 +74,7 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
+    !production && serve(),
     production && terser()
   ]
 };
